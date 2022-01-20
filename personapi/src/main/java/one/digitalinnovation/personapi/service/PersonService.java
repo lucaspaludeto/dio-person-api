fcff2ca97f7e2ebc794913358d92862bc98af51d
Lucas Paludeto <lucas.paludeto@hotmail.com>
2022-01-20 19:54:06 -0300

    to discard changes in working directory

--- personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,11 +3,11 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,5 +38,12 @@ public class PersonService {
         return allPeople.stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    public PersonDTO findBy(Long id) throws PersonNotFoundException {
+       Person person = personRepository.findById(id)
+                .orElseThrow(()-> new PersonNotFoundException(id));
+
+                return personMapper.toDTO(person);
     }
 }
