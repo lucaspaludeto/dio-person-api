147e34345be251b1dabb1bf0480639b77b3e0ab9
Lucas Paludeto <lucas.paludeto@hotmail.com>
2022-01-20 17:57:16 -0300

    Developed person create operation

--- personapi/src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -1,12 +1,17 @@
 package one.digitalinnovation.personapi.entity;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
@@ -25,4 +30,6 @@ public class Phone {
 
     @Column(nullable = false)
     private String number;
+
+
 }
