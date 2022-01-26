302e7f8eccce0deecde7e2d8058a04299d1294bf
Lucas Paludeto <lucas.paludeto@hotmail.com>
2022-01-26 11:00:10 -0300

    Developed delete by Id opperation

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -35,7 +35,13 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public PersonDTO findBy(@PathVariable Long id) throws PersonNotFoundException {
-        return personService.findBy(id);
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 
 
